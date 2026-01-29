8dc987a26039f8e7eecabe2f207a7970a6fc0815
Németh Benedek <53515381+Binibeno@users.noreply.github.com>
2026-01-29 02:24:13 +0100

    Fixed import errors

--- packages/@jxa/global-type/src/index.d.ts
+++ packages/@jxa/global-type/src/index.d.ts
@@ -1,23 +1,25 @@
-import type {
-    Application,
-    ObjectSpecifier,
-    Automation
+import {
+    Application as JXAApplication,
+    ObjectSpecifier as JXAObjectSpecifier,
+    Automation as JXAAutomation
 } from "@jxa/types";
 
 declare global {
-    const Application: typeof Application;
-    const Automation: typeof Automation;
-    const ObjectSpecifier: typeof ObjectSpecifier;
+    interface GlobalThis {
+        Application: typeof JXAApplication;
+        Automation: typeof JXAAutomation;
+        ObjectSpecifier: typeof JXAObjectSpecifier;
 
-    function Path(name: string): object;
+        Path: (name: string) => object;
 
-    /**
-     * Pause for a fixed amount of time
-     * @param delay the number of seconds to delay (default is 0)
-     *
-     */
-    function delay(delay?: number): void;
+        /**
+         * Pause for a fixed amount of time
+         * @param delay the number of seconds to delay (default is 0)
+         *
+         */
+        delay: (delay?: number) => void;
 
-    const ObjC: any;
-    const $: any;
+        ObjC: any;
+        $: any;
+    }
 }
